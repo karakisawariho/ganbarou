80fe2a1415590fc8c22643b94fee658e213fdb65
karakisawariho <167832826+karakisawariho@users.noreply.github.com>
2024-12-10 09:52:54 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementById('sumo').src='./image/opantyuusagi.jpg';}
-function mes2(){document.getElementById('sumo').src='./image/opantyuusagi3.jpg';}
+function imgchange5(){document.getElementById('opantyuusagi').src='./image/opantyuusagi.jpg';}
+function mes2(){document.getElementById('opantyuusagi3').src='./image/opantyuusagi3.jpg';}
