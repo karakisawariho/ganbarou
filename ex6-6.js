b6ef316bf1ae900a65cab08df62ff2999042e8f7
karakisawariho <167832826+karakisawariho@users.noreply.github.com>
2024-12-10 09:21:33 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,7 +1,4 @@
-var who66 ='world';
-function sayhello66(){
-alert('Hello,'+who66+'!');
-}
-function taro66(){who66='太郎';}
-function hanako66(){who66='花子';}
-function gakita66(){alert(who66+'が来た');}
+function sayhello61(){
+  alert('Hello,Function!');}
+function sayhello61(){
+  alert('saygoodbye,Goodbye!');}
