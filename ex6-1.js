6dfbbb4552a14ecb43b53cad363ac89155e21ad9
karakisawariho <167832826+karakisawariho@users.noreply.github.com>
2024-12-10 09:21:12 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -1,2 +1,5 @@
-function sayhello61(){alert('Hello,Function!');}
-function saygoodbye61(){alert('Goodbye!');}
+function sayhello61(){
+  alert('Hello,Function!');}
+function sayhello61(){
+  alert('saygoodbye,Goodbye!');}
+
