0cd9c5aca72aa0eec20193bc492fa532d09d4697
karakisawariho <167832826+karakisawariho@users.noreply.github.com>
2024-12-10 09:43:28 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementById('sumo').src='./image/sumo1.jpg';}
-function mes2(){document.getElementById('sumo').src='./image/sumo2.jpg';}
+function imgchange5(){document.getElementById('sumo').src='./image/opantyuusagi.jpg';}
+function mes2(){document.getElementById('sumo').src='./image/opantyuusagi3.jpg';}
