a8ec1e99ca5fbc19b93e06092d85d20dceb3c232
karakisawariho <167832826+karakisawariho@users.noreply.github.com>
2025-01-31 20:22:39 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,3 +1,3 @@
 function gazo688() {
-    document.getElementById('who688').innerHTML = '<img src="./image/hiraku.png" alt="オン">';
+    document.getElementById('who688').innerHTML = '<img src="./image/various_arukuma6.png" alt="オン">';
 }
