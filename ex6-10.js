5e794ff382cbc4a42495d9ecdff8c1fb2f81e77a
karakisawariho <167832826+karakisawariho@users.noreply.github.com>
2025-01-31 20:30:39 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,3 +1,3 @@
-function imgchange1(){document.getElementById('mark').src='./image/arukuma.png';}
-function imgchange2(){document.getElementById('mark').src='./image/various_arukuma6.png';}
+function imgchange1(){document.getElementById('mark').src='./image/various_arukuma6.png';}
+function imgchange2(){document.getElementById('mark').src='./image/arukuma.png';}
 
